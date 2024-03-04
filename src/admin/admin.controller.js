e080219439ae81feb3823211e20f0b10414b433b
bgomez-2022208 <bgomez-2022208@kinal.edu.gt>
2024-03-04 17:19:42 -0600

    se avanzo en admin

---
+++ src/admin/admin.controller.js
@@ -0,0 +1,48 @@
+'use strict';
+
+import {response, request } from "express";
+import bcryptjs from 'bcryptjs';
+import Admin from './admin';
+
+
+// Metodo para obtener al administrador
+export const getAdminsById = async (req, res) => {
+    const { id } = req.params;
+    const admin = await Admin.findOne({ _id: id});
+
+    res.status(200).json({
+        admin,
+    });
+}
+
+//Crear
+
+export const createAdmin = async (req, res) => {
+    const { nombre, correo, password} = req.body;
+    const admin = new Admin ({ nombre, correo, password, role: "ADMIN_ROLE"});
+    admin.password ="ADMIN_ROLE";
+
+    const salt = bcryptjs.genSaltSync();
+    admin.password = bcryptjs.hashSync(password, salt);
+
+    await admin.save();
+
+    res.status(200).json({
+        admin,
+    });
+}
+
+//Update
+
+export const adminPut = async (req, res = response) => {
+    const { id } = req.params;
+    const {_id, ...resto} = req.body;
+    await Admin.findByIdAndUpdate(id, resto);
+
+    const admin = await Admin.findOne({_id: id});
+
+    res.status(200).json({
+        msg: 'Administrador actualizado',
+        admin,
+    });
+}
---
+++ src/admin/admin.js
@@ -0,0 +1,33 @@
+import mongoose from 'mongoose';
+
+const AdminSchema = mongoose.Schema({
+    nombre: {
+        type: String,
+        required: [true, "The name is required"],
+    },
+    correo: {
+        type: String,
+        required: [true, "Email is mandatory"],
+        unique: true,
+    },
+    password: {
+        type: [true, "Password is required"],
+    },
+    role:{
+        type: String,
+        required: true,
+        enum: ["ADMIN_ROLE","CUSTOMER_ROLE"],
+    },
+    estado: {
+        type: Boolean,
+        default: true,
+    },
+});
+
+/*AdminSchema.methods.toJSON = function(){
+    const { __v, password, _id, ...admin} = this.toObject(),
+    admin.uid = id;
+    return admin;
+}*/
+
+export default mongoose.model('Admin', AdminSchema);
